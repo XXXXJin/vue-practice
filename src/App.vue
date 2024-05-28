e47e5b4c387d0293ef14c8189e65bbefd3b6f8a6
XXXXJin <jinhaixu@hotmail.co.jp>
2024-05-28 15:33:35 +0900

    add nav bar and pageviue component

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import NavBar from '@/components/NavBar.vue'
+import PageView from './components/PageView.vue'
+import { ref } from 'vue'
+
+const pages = ref([
+  { text: 'Home', url: 'home.html' },
+  { text: 'About', url: 'home.html' },
+  { text: 'Services', url: 'home.html' },
+  { text: 'Pricing', url: 'home.html' },
+  { text: 'Contact', url: 'home.html' }
+])
+const pageIndex = ref(0)
+
+const changePageIndex = (index) => {
+  console.log(index)
+  pageIndex.value = index
+}
+console.log(pageIndex.value)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-
-    <div class="">test</div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <NavBar
+    v-bind:pages="pages"
+    v-bind:pageIndex="pageIndex"
+    v-bind:changePageIndex="changePageIndex"
+  />
+  <PageView />
+  <h1>Hello world!!!</h1>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/PageView.vue
@@ -0,0 +1,33 @@
+<script>
+// import { ref } from 'vue'
+// const textContent = ref('hi')
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      textContent: 'hi there',
+      style: {
+        color: 'red',
+        fontSize: '30px',
+        num: 3
+      },
+      names: ['Bruce', 'clark', 'diana'],
+      name: 'jin'
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = 'bat man'
+    }
+  }
+}
+</script>
+<template>
+  <!-- <h1 v-bind:style="{ color: 'blue', ...style, color: 'green' }">{{ textContent }}</h1>
+  <p v-if="style.color === 'red'">the color is indeed red</p>
+  <p v-else>the color is not red</p> -->
+  <!-- <p v-for="name in names" v-bind:key="name">{{ name }}</p> -->
+  <p>{{ name }}</p>
+  <button v-on:click="changeName">change name</button>
+</template>
