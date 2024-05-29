f89f2c39c2a11561a2b94f9a562156724f37d6c0
XXXXJin <jinhaixu@hotmail.co.jp>
2024-05-29 09:47:26 +0900

    learning about form submit

--- src/components/PageView.vue
+++ src/components/PageView.vue
@@ -7,21 +7,36 @@ export default {
   data() {
     return {
       formValue: {
-        name: ''
+        name: '',
+        location: ''
       }
     }
   },
   methods: {
     changeName() {
       this.name = 'bat man'
+    },
+    handleSubmit() {
+      console.log(this.formValue)
     }
   }
 }
 </script>
 <template>
-  <p>{{ formValue.name }}</p>
-  <form action="">
-    <label for="name">Name</label>
-    <input type="text" id="name" v-model="formValue.name" />
+  <p>{{ JSON.stringify(formValue, null, 2) }}</p>
+  <form v-on:submit.prevent="handleSubmit">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" class="border" v-model="formValue.name" />
+    </div>
+    <div>
+      <select name="location" id="location" v-model="formValue.location">
+        <option value="">select the location</option>
+        <option value="tokyo">tokyo</option>
+        <option value="london">london</option>
+        <option value="new york">new york</option>
+      </select>
+    </div>
+    <button>submit</button>
   </form>
 </template>
