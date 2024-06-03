cf1855355e585c2a7c68387a4c4fc6f68ea3de23
XXXXJin <jinhaixu@hotmail.co.jp>
2024-06-03 14:05:00 +0900

    new weather app

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,85 @@
-<script>
-import NavBar from '@/components/NavBar.vue'
-import PageView from './components/PageView.vue'
-import PopUp from '@/components/PopUp.vue'
-import SlotComponent from '@/components/SlotComponent.vue'
-
-export default {
-  name: 'App',
-  data() {
-    return {
-      pages: [
-        { text: 'Home', url: 'home.html' },
-        { text: 'About', url: 'home.html' },
-        { text: 'Services', url: 'home.html' },
-        { text: 'Pricing', url: 'home.html' },
-        { text: 'Contact', url: 'home.html' }
-      ],
-      pageIndex: 0,
-      name: 'jinahixu',
-      showPopup: false
-    }
-  },
-  methods: {
-    changePageIndex(index) {
-      this.pageIndex = index
-    },
-    closePopup(secret) {
-      if (secret === 'secret') this.showPopup = false
-    }
-  },
-  components: {
-    NavBar,
-    PageView,
-    PopUp,
-    SlotComponent
-  },
-  provide() {
-    return {
-      username: this.name
-    }
-  }
-}
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <NavBar
-    v-bind:pages="pages"
-    v-bind:pageIndex="pageIndex"
-    v-bind:changePageIndex="changePageIndex"
-  />
-  <PageView />
-  <!-- <h1 v-show="showPopup" v-on:closePopup="showPopup = false">Hello world!!!</h1> -->
-  <PopUp v-show="showPopup" v-on:closePopup="closePopup"></PopUp>
-  <button v-on:click="showPopup = true">show the pop</button>
-  <SlotComponent>
-    <template v-slot:header>header slot</template>
-    <template v-slot:default>default slot</template>
-    <template v-slot:footer>footer slot</template>
-  </SlotComponent>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
