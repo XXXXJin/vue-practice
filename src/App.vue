f56bd76d8d5f933675318bd521fd943f6c4a97d6
XXXXJin <jinhaixu@hotmail.co.jp>
2024-06-03 22:13:14 +0900

    add new route and links to the route

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <SiteNatigation />
-  <RouterView />
+  <!-- <SearchInput v-on:add-location="addLocation" /> -->
+  <RouterView v-on:add-location="addLocation" />
 </template>
 
 <script setup>
 import SiteNatigation from '@/components/SiteNavigation.vue'
+// import SearchInput from '@/components/SearchInput.vue'
+import {ref} from 'vue'
+
+const locations = ref([])
+
+const addLocation = (location) => {
+  locations.value.push(location)
+  console.log(location.value, 123)
+}
 
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup>
-import axios from 'axios';
-import {reactive} from 'vue'
+import axios from 'axios'
+import { useRouter } from 'vue-router'
+import { reactive } from 'vue'
+
 const searchTerm = reactive({
   query: '',
   timeout: null,
@@ -8,52 +10,83 @@ const searchTerm = reactive({
   errorFlag: false
 })
 
+const router = useRouter()
+
+// const emit = defineEmits(['send-weather-data'])
+
+const previewCity = (id) => {
+  console.log(id)
+  router.push({
+    name: 'cityView',
+    params: { id }
+  })
+}
+
 const handleSearch = () => {
-  clearTimeout(searchTerm.timeout);
+  clearTimeout(searchTerm.timeout)
   searchTerm.timeout = setTimeout(() => {
-    if(searchTerm.query !== '') {
-      axios.get(`http://api.weatherapi.com/v1/search.json?key=01310cb2121b4b32ab181800240306&q=${searchTerm.query}`).then(result => {
-        searchTerm.result = result.data
-        console.log(result.data)
-      }).catch((err) => {
-        searchTerm.errorFlag = true;
-      });
+    if (searchTerm.query !== '') {
+      axios
+        .get(
+          `http://api.weatherapi.com/v1/search.json?key=01310cb2121b4b32ab181800240306&q=${searchTerm.query}`
+        )
+        .then((result) => {
+          searchTerm.result = result.data
+          console.log(result.data)
+        })
+        .catch((err) => {
+          searchTerm.errorFlag = true
+          console.log(err)
+        })
     } else {
       searchTerm.result = null
     }
-  }, 700);
+  }, 700)
 }
 
-const getWeather = (id) => {
-  axios.get(`http://api.weatherapi.com/v1/forecast.json?key=01310cb2121b4b32ab181800240306&q=id:${id}&days=3&aqi=no&alerts=no`).then((result) => {
-    console.log(result.data)
-  }).catch((err) => {
-    console.log(err)
-  });
-}
+// const getWeather = (id) => {
+//   axios.get(`http://api.weatherapi.com/v1/forecast.json?key=01310cb2121b4b32ab181800240306&q=id:${id}&days=3&aqi=no&alerts=no`).then((result) => {
+//     emit('send-weather-data', result.data)
+//     console.log(result.data)
+//   }).catch((err) => {
+//     console.log(err)
+//   })
+// }
 </script>
 
 <template>
   <div class="container">
     <form class="mt-6">
-      <div class="bg-white border  rounded-lg shadow-lg flex items-center">
-        <i class="fa-solid fa-magnifying-glass p-2 "></i>
+      <div class="bg-white border rounded-lg shadow-lg flex items-center">
+        <i class="fa-solid fa-magnifying-glass p-2"></i>
         <input
           type="text"
           placeholder="Search for a place"
-          class="rounded-r-lg p-2 border-0 outline-0 focus:ring-2  ring-inset w-full"
+          class="rounded-r-lg p-2 border-0 outline-0 focus:ring-2 ring-inset w-full"
           v-model="searchTerm.query"
           v-on:input="handleSearch"
         />
       </div>
-      <p v-if="searchTerm.errorFlag" class="text-red-600 text-xs p-2">問題が発生しました。しばらくしてから再度お試しください。</p>
-      <p v-if="!searchTerm.errorFlag && searchTerm.result && searchTerm.result.length === 0" class="text-red-600 text-xs p-2">結果がありませんでした。違うキーワードで再度検索してください。</p>
+      <p v-if="searchTerm.errorFlag" class="text-red-600 text-xs p-2">
+        問題が発生しました。しばらくしてから再度お試しください。
+      </p>
+      <p
+        v-if="!searchTerm.errorFlag && searchTerm.result && searchTerm.result.length === 0"
+        class="text-red-600 text-xs p-2"
+      >
+        結果がありませんでした。違うキーワードで再度検索してください。
+      </p>
     </form>
 
     <div class="bg-white my-2 rounded-lg shadow-lg">
       <template v-if="searchTerm && searchTerm.length !== 0">
         <div v-for="location in searchTerm.result" :key="location.id">
-          <button class="px-3 my-2 hover:font-bold w-full text-left" v-on:click="getWeather(location.id)">{{ `${location.country}, ${location.region}, ${location.name}` }}</button>
+          <button
+            class="px-3 my-2 hover:font-bold w-full text-left"
+            v-on:click="previewCity(location.id)"
+          >
+            {{ `${location.country}, ${location.region}, ${location.name}` }}
+          </button>
         </div>
       </template>
     </div>
